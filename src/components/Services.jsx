26441cdd7cfff03c84658048dc19404013a36ea8
Victor Ubah <99929155+OfficiallyVictor@users.noreply.github.com>
2025-09-27 23:01:24 -1100

    sserives

---
+++ src/components/Services.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const Services = () => {
+
+    const servicesData = [
+        {
+            title: 'Advertising',
+            description: 'We turn bold ideas into powerful digital solutions that connect, engage...',
+            icon: assets.ads_icon
+        },
+        {
+            title: 'Content Marketing',
+            description: 'We help you execute your plan and deliver results',
+            icon: assets.marketing_icon
+        },
+        {
+            title: 'Content Writing',
+            description: 'We help you create a marketing strategy that drives results',
+            icon: assets.content_icon
+        },
+        {
+            title: 'Social media',
+            description: 'We help you build a strong social media presence and engage with your audience',
+            icon: assets.social_icon
+        },
+    ]
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Services
